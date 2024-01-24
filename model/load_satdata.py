67f8e28af116c9639fec13729655bcea09daad74
Joel Walsh <joelawalsh@gmail.com>
2024-01-24 08:23:35 -0800

    args back to default

--- model/load_satdata.py
+++ model/load_satdata.py
@@ -270,11 +270,11 @@ def config_parser():
                         help='will take every 1/N images as LLFF test set, paper uses 8')
 
     # logging/saving options
-    parser.add_argument("--i_print",   type=int, default=10,
+    parser.add_argument("--i_print",   type=int, default=1000,
                         help='frequency of console printout and metric loggin')
     parser.add_argument("--i_img",     type=int, default=500,
                         help='frequency of tensorboard image logging')
-    parser.add_argument("--i_weights", type=int, default=200,
+    parser.add_argument("--i_weights", type=int, default=1000,
                         help='frequency of weight ckpt saving')
     parser.add_argument("--i_testset", type=int, default=500000,
                         help='frequency of testset saving')
